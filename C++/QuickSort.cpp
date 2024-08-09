0ae24a87e471eb036ac2febe81f5574457f06d93
Luis <luisgmgomes@gmail.com>
2024-08-09 13:23:22 -0300

    quicksort cpp

--- C++/QuickSort.cpp
+++ C++/QuickSort.cpp
@@ -16,17 +16,17 @@ void quickSort(std::vector<int> &array, int *start, int *end)
 
 int *partition(std::vector<int> &array, int *start, int *end)
 {
-	int pivot = array[*end];
+	int pivot = *end;
 	int *i = start - 1;
 
-	for (int *j = start; j < &array[*end]; j++)
+	for (int *j = start; j < end; j++)
 	{
 		if (*j < pivot)
 		{
 			i++;
-			int temp = *j;
-			*j = *i;
-			*i = temp;
+			int temp = *i;
+			*i = *j;
+			*j = temp;
 		}
 	}
 	i++;
@@ -43,10 +43,7 @@ int main()
 	quickSort(array, &array[0], &array[array.size() - 1]);
 
 	for (int num : array)
-	{
-		std::cout << num << " ";
-	}
-	std::cout << std::endl;
+		std::cout << num << ", ";
 
 	return 0;
 }
